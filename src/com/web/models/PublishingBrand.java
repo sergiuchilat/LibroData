985fd788ec71141ee0fd1c056c296542ca6b6b1e
gorincioifelicia <felicia.gorincioi@stud.acs.upb.ro>
2021-11-25 12:03:22 +0200

    am adaugat getteri si setteri

--- src/com/web/models/PublishingBrand.java
+++ src/com/web/models/PublishingBrand.java
@@ -6,4 +6,37 @@ public class PublishingBrand {
     public int ID;
     public String name;
     public List<Book> books;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "PublishingBrand{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+    }
 }
